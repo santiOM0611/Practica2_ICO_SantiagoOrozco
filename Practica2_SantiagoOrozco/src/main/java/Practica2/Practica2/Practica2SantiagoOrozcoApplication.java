255b050ee62282d08c1723db85fad4ff393d7c20
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-02-03 19:53:28 -0600

    static.css
    Se configuraron los estilos

---
+++ Practica2_SantiagoOrozco/src/main/java/Practica2/Practica2/Practica2SantiagoOrozcoApplication.java
@@ -0,0 +1,13 @@
+package Practica2.Practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2SantiagoOrozcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2SantiagoOrozcoApplication.class, args);
+	}
+
+}
---
+++ Practica2_SantiagoOrozco/src/test/java/Practica2/Practica2/Practica2SantiagoOrozcoApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica2.Practica2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2SantiagoOrozcoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
